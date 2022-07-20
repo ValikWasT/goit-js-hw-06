06ebda7a247f50860574bf3d401bb64602e93810
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-07-20 19:35:18 +0300

    did task 1

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+// const numberOfCategories =
+//   document.querySelectorAll("#categories .item").length;
+// console.log("Number of categories: ", numberOfCategories);
+
+const arrayOfCategories = document.querySelectorAll("#categories .item");
+
+console.log("Number of categories: ", arrayOfCategories.length);
+
+const allCategories = [...arrayOfCategories].map((category) => {
+  console.log("Category: ", category.querySelector("h2").textContent);
+  console.log("Elements: ", category.querySelectorAll("li").length);
+});
