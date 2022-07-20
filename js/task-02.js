ac3263b26d5f142a7d1cb39d5ba2f6c57496dced
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-07-20 19:54:42 +0300

    did task 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const allEl = [];
+
+const oneEl = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  allEl.push(itemEl);
+});
+// console.log(...allEl);
+const list = document.querySelector("#ingredients");
+list.append(...allEl);
+// console.log(list);
