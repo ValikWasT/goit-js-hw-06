b5d397af7b2ed5ab14df5857fad94b178ea19d51
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-07-21 14:00:18 +0300

    did task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+const formValue = {
+  email: "",
+  password: "",
+};
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Помилка! Форма не заповнена.");
+  } else {
+    formValue.email = email.value;
+    formValue.password = password.value;
+    console.log(formValue);
+    event.currentTarget.reset();
+  }
+});
