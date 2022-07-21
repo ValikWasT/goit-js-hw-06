05dc00395478cd688ea78552f06f77a31ff8f135
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-07-21 14:14:30 +0300

    did task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const buttonRef = document.querySelector(".change-color");
+const spanColorRef = document.querySelector("p span");
+buttonRef.addEventListener("click", () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanColorRef.textContent = getRandomHexColor();
+});
